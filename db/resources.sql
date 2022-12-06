5d286345e17e2d6c65ace73eace3206610a0d123
tgn <trwygon@outlook.pt>
2022-12-06 10:22:16 +0000

    database

---
+++ db/resources.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE Resources
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(50) NULL, 
+    [Content] VARCHAR(50) NULL, 
+    [CreationDate] DATETIME NOT NULL, 
+    [Parent] INT FOREIGN KEY REFERENCES Resources(Id), 
+    [Event] VARCHAR(50) NULL,
+    [Endpoint] VARCHAR(50) NULL,
+	[Type] VARCHAR(50) NOT NULL,
+)
