3621b4c444345529297306f1e2711fe19cda5d3f
tgn <trwygon@outlook.pt>
2022-12-28 14:04:43 +0000

    update db

--- SOMIOD/App_Data/resources.sql
+++ SOMIOD/App_Data/resources.sql
@@ -1,11 +1,13 @@
-﻿CREATE TABLE Resources
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Name] VARCHAR(50) NULL, 
-    [Content] VARCHAR(50) NULL, 
-    [CreationDate] DATETIME NOT NULL, 
-    [Parent] INT FOREIGN KEY REFERENCES Resources(Id), 
-    [Event] VARCHAR(50) NULL,
-    [Endpoint] VARCHAR(50) NULL,
-	[Type] VARCHAR(50) NOT NULL,
-)
+﻿CREATE TABLE [dbo].[Resources] (
+    [Id]           INT          NOT NULL,
+    [Name]         VARCHAR (50) NOT NULL UNIQUE,
+    [Content]      VARCHAR (50) NULL,
+    [CreationDate] DATETIME     NOT NULL,
+    [Parent]       INT          NULL,
+    [Event]        VARCHAR (50) NULL,
+    [Endpoint]     VARCHAR (50) NULL,
+    [Type]         VARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    FOREIGN KEY ([Parent]) REFERENCES [dbo].[Resources] ([Id])
+);
+
