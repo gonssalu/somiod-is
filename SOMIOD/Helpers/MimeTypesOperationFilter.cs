3287c17bf43d88b3cf20f569c2b9479f32d2002c
tgn <trwygon@outlook.pt>
2022-12-29 22:02:21 +0000

    Fixed swagger bug
    https://github.com/domaindrivendev/Swashbuckle.WebApi/issues/691

---
+++ SOMIOD/Helpers/MimeTypesOperationFilter.cs
@@ -0,0 +1,21 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+namespace SOMIOD.Helpers
+{
+    public class MimeTypesOperationFilter : IOperationFilter
+    {
+        private static readonly string[] SupportedAccepts = new[] { "application/xml" };
+        private static readonly string[] SupportedContentTypes = new[] { "application/xml" };
+
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            operation.consumes = SupportedAccepts;
+            operation.produces = SupportedContentTypes;
+        }
+    }
+}
