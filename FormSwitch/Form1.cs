4fd7ed007038afcc8dd3076e79eda2385efb85b9
Alexandre <alexandrejeronimo2001@gmail.com>
2023-01-02 15:43:21 +0000

    FormSwitch w/ mqtt package

---
+++ FormSwitch/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt;
+
+
+namespace FormSwitch
+{
+    public partial class Form1 : Form
+    {
+        private MqttClient _mClient;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
