f3f0824bf0b67391b6fa3f9676f6a2d39434a485
Alexandre <alexandrejeronimo2001@gmail.com>
2023-01-03 09:32:22 +0000

    formlightbulb rename

--- FormLightBulb/FormLightBulb.Designer.cs
+++ FormLightBulb/FormLightBulb.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FormLightBulb
 {
-    partial class Form1
+    partial class FormLightBulb
     {
         /// <summary>
         /// Required designer variable.
@@ -28,35 +28,33 @@
         private void InitializeComponent()
         {
             this.pbLightbulb = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pbLightbulb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.pbLightbulb)).BeginInit();
             this.SuspendLayout();
             // 
             // pbLightbulb
             // 
             this.pbLightbulb.Image = global::FormLightBulb.Properties.Resources.light_bulb_off;
             this.pbLightbulb.Location = new System.Drawing.Point(16, 15);
-            this.pbLightbulb.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pbLightbulb.Margin = new System.Windows.Forms.Padding(4);
             this.pbLightbulb.Name = "pbLightbulb";
             this.pbLightbulb.Size = new System.Drawing.Size(296, 283);
             this.pbLightbulb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbLightbulb.TabIndex = 1;
             this.pbLightbulb.TabStop = false;
             // 
-            // Form1
+            // FormLightBulb
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(328, 313);
             this.Controls.Add(this.pbLightbulb);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing_1);
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FormLightBulb";
+            this.Text = "Light Bulb";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormLightBulb_FormClosing);
             this.Shown += new System.EventHandler(this.FormLightBulb_Shown);
-            ((System.ComponentModel.ISupportInitialize)(this.pbLightbulb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.pbLightbulb)).EndInit();
             this.ResumeLayout(false);
-
         }
 
         #endregion
